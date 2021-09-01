458ac1b075ad3c5b597eb3f78e7badb607b5ea6a
hoanguye@local <hdnguyen0305@gmail.com>
2021-09-01 10:22:15 +0700

    order page

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -4,31 +4,12 @@ import { useSelector } from 'react-redux';
 import Login from '../Login/Login';
 import orderData from "../../data/orders.json";
 import OrderData from "./OrderData.js";
-
-
-// export function Order() {
-//   const [orders, setOrders] = useState(orderData);
-//   return (
-//   <div>
-//       <div className="bg-white w-screen h-screen ">
-//         <section className="bg-white py-8 mt-0">
-//           <div className="container mx-auto flex items-center flex-wrap pt-4 pb-12">
-
-//             <a className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl " href="#">
-//               Your Order
-//             </a>
-//             <div className="w-full md:w-full p-6 flex flex-col items-center space-y-5">
-//               {orders.detail.content.map((obj, index) => <OrderData key={index} {...obj} />)}
-//             </div>
-//           </div>
-//         </section>
-//       </div>
-//     </div>
-// );
-// }
+import { v4 } from "uuid";
 
 export function Order() {
   const [orders, setOrders] = useState(orderData);
+  const state = useSelector(state=>state.auth);
+  if (!state.user) return (<Login />);
   return (
   <div>
       <div className="w-screen h-screen ">
@@ -49,8 +30,7 @@ export function Order() {
 }
 
 // export function Order() {
-//   // const state = useSelector(state=>state.auth);
-//   const state = {"user": "haong"};
+//   const state = useSelector(state=>state.auth);
 //   const [data, setData] = useState();
 //   const [error, setError] = useState();
 //   const [loading, setLoading] = useState(true);
@@ -63,15 +43,15 @@ export function Order() {
 //               {
 //                 // "requestId": "{{$guid}}",
 //                 // "requestTime": "{{$timestamp}}",
-//                 "requestId": "b5677123-75cf-4dbf-a82e-8e6c4133746c",
-//                 "requestTime": "1630377731",
+//                 "requestId": v4(),
+//                 "requestTime": Date.now(),
 //                 "targetId": "BIC",
 //                 "type": "BICTravelInsurance",
 //                 "detail": {
 //                   "orders": {
 //                         "ordBillFirstName": "12",
 //                         "ordBillMobile": "097453686312",
-//                         "ordBillStreet1": "TTP, Đan Phượng, Hà Nội",
+//                         "ordBillStreet1": "TTP, Đn Phượng, Hà Nội",
 //                         "ordBillEmail": "nghiemxuanhop98@gmail.com",
 //                         "ordDate": "2020-02-17T10:37:22",
 //                         "ordStatus": "1",
@@ -103,14 +83,11 @@ export function Order() {
 //             ),
 //             headers: {
 //               'Content-Type': 'application/json',
-//                 // Authorization: `Bearer` + state.user.access_token
-//                 'Authorization': `Bearer ` + 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJodW5nIiwic2NvcGUiOlsicmVhZCJdLCJpc3MiOiJodHRwOi8vc21hcnRtYXJrZXQuY29tIiwiZXhwIjoxNjMwMzgxMjE2LCJhdXRob3JpdGllcyI6WyJyZWFkIl0sImp0aSI6IjRhNGI2ODAwLTVhMDAtNGJiMi04NjFhLWYzMjU2MGNhNjFhMiIsImNsaWVudF9pZCI6ImNsaWVudDMifQ.ONlEwWMF7ydH-8rwM2YO_PpwY_jilGMxAtSlwfQTlSHza0yOXP7TPgCIE3ZswaHTiv3VkSJ5SF7tmR6NwvND48HcII_T6gS9llhCSSNr0w3eUAmEt_vbekl5AW4RzNSzvITicRUF0897ChpO3AqrUqLs8zAf5DNAnXy85nHBRgAzTCushKglCizFzDyL3bVeaval2WNsRY63qSen7e4xv9T6q9iy0dUEe0YxaZtsIHIYBtZ08bdniH2q4KWWUwOgzBy_KJUAOFjABC3bJA6U4kvfocv9ED9RuiiE_SyGJwEoCLu0Y9ghn6pFS7_gYPJf4Ea_B1vP4CpIo0VPLwGdGw'
+//                 'Authorization': `Bearer ` + state.user.access_token
 //             }
-//             }).then(
-//                 (response) => (
-//                     console.log(response)
-//                 )
-//             )
+//             })
+//       .then(response => response.text())
+//       .then(result => console.log(result))
 //             .catch(
 //                 error => {
 //                     console.log(error);
